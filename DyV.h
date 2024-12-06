0dad439008f39af44e5fee609ac8fe5227cfce79
Pablo C P <pcobper@upv.edu.es>
2024-12-06 01:00:09 +0100

    implementado el cronómetro y los distintos tipos de QuickSort

--- DyV.h
+++ DyV.h
@@ -1,4 +1,24 @@
 #include <vector>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//función para imprimir vectores
+template <typename T>
+void imprimir_vector(T v[], int tamanyo){
+
+	cout << "{";
+
+	for(int i = 0; i<tamanyo; i++){
+                cout << v[i];
+                if(i < tamanyo-1){
+                        cout << ", ";
+                }else{
+                        cout << "}" << endl;
+                }
+        }
+}
+
 
 template <typename T>
 int BusquedaBinaria(T x, T v[], int ini, int fin){
@@ -73,3 +93,76 @@ void QuickSort(T v[], int ini, int fin){
         }
 }
 
+
+template <typename T>
+int Partition_mid(T v[], int ini, int fin) {
+    
+    int mid = ini + (fin - ini) / 2;
+    T x = v[mid];
+
+
+    T aux = v[mid];
+    v[mid] = v[fin];
+    v[fin] = aux;
+
+    int i = ini;
+
+    for (int j = ini; j <= fin - 1; j++) {
+        if (v[j] <= x) {
+            aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            i++;
+        }
+    }
+
+    aux = v[i];
+    v[i] = v[fin];
+    v[fin] = aux;
+
+    return i;
+}
+
+template <typename T>
+void QuickSort_mid(T v[], int ini, int fin){
+        if(ini < fin){
+                int pivot = Partition_mid(v, ini, fin);
+
+                QuickSort(v, ini, pivot-1);
+                QuickSort(v, pivot+1, fin);
+        }
+}
+
+
+
+template <typename T>
+int Partition_ini(T v[], int ini, int fin) {
+    T x = v[ini];
+    int i = ini + 1;
+
+    for (int j = ini + 1; j <= fin; j++) {
+        if (v[j] <= x) {
+            T aux = v[i];
+            v[i] = v[j];
+            v[j] = aux;
+            i++;
+        }
+    }
+
+    
+    T aux = v[i - 1];
+    v[i - 1] = v[ini];
+    v[ini] = aux;
+
+    return i - 1;
+}
+
+template <typename T>
+void QuickSort_ini(T v[], int ini, int fin){
+        if(ini < fin){
+                int pivot = Partition_ini(v, ini, fin);
+
+                QuickSort(v, ini, pivot-1);
+                QuickSort(v, pivot+1, fin);
+        }
+}
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -3,9 +3,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 using namespace std;
-
-#define TAMANYO 10
 
 int main(){
 
@@ -15,31 +14,43 @@ int main(){
 	srand(time(NULL));
 
 	//generamos un vector aleatorio desordenado
-	int vector_desordenado[10];
+	int vector_desordenado[10], vector_desordenado2[10], vector_desordenado3[10];
 
-	cout << "El vector generado es: {";
+	cout << "El vector generado es: ";
 
 	for(int i = 0; i<10; i++){
 		vector_desordenado[i] = rand() % 10;
-		cout << vector_desordenado[i];
-		if(i < 9){
-			cout << ", ";
-		}else{
-			cout << "}" << endl;
-		}
+		vector_desordenado2[i] = vector_desordenado[i];
+		vector_desordenado3[i] = vector_desordenado[i];
 	}
-
+	imprimir_vector(vector_desordenado, 10);
+	
+	auto start = chrono::system_clock::now();
 	//ordenamos el vector
-	QuickSort(vector_desordenado, 0, 10);
+	QuickSort(vector_desordenado, 0, 9);
+	auto end = chrono::system_clock::now();
+	chrono::duration<float, milli> duration = end - start;
+	cout << "Duración QuickSort (pivote fin): " << duration.count() << "s" << std::endl;
 
-        for(int i = 0; i<10; i++){
-                cout << vector_desordenado[i];
-                if(i < 9){
-                        cout << ", ";
-                }else{
-                        cout << "}" << endl;
-                }
-        }
+	imprimir_vector(vector_desordenado, 10);
+
+	auto start = chrono::system_clock::now();
+        //ordenamos el vector
+        QuickSort_mid(vector_desordenado2, 0, 9);
+        auto end = chrono::system_clock::now();
+        chrono::duration<float, milli> duration = end - start;
+        cout << "Duración QuickSort (pivote mid): " << duration.count() << "s" << std::endl;
+
+	imprimir_vector(vector_desordenado2, 10);
+
+	auto start = chrono::system_clock::now();
+        //ordenamos el vector
+        QuickSort_ini(vector_desordenado3, 0, 9);
+        auto end = chrono::system_clock::now();
+        chrono::duration<float, milli> duration = end - start;
+        cout << "Duración QuickSort (pivote ini): " << duration.count() << "s" << std::endl;
+
+	imprimir_vector(vector_desordenado3, 10);
 
 	//buscamos un número aleatorio a ver si está
 	int n = rand() % 10;
